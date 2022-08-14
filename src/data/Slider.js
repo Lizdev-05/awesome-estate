4b7c57e96b4f56e05b4840a76e7a7f4a30624eda
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-14 20:34:45 +0100

    add slider images

---
+++ src/data/Slider.js
@@ -0,0 +1,80 @@
+import slider1 from '../images/slider1.jpg';
+import slider2 from '../images/slider2.jpg';
+import slider3 from '../images/slider3.jpg';
+import slider4 from '../images/slider4.jpg';
+import slider5 from '../images/slider5.jpg';
+import slider6 from '../images/slider6.jpg';
+import slider7 from '../images/slider7.jpg';
+import slider8 from '../images/slider8.jpg';
+
+export const Slider = [
+  {
+    title: 'Lake House in Westport, NY',
+    price: '$937,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider1,
+    alt: 'House',
+  },
+  {
+    title: 'Bungalow in Lake Placid, NY',
+    price: '$520,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider2,
+    alt: 'House',
+  },
+  {
+    title: 'Estate in Lake George, NY',
+    price: '$4,198,100',
+    path: '/homes',
+    label: 'View Home',
+    image: slider3,
+    alt: 'House',
+  },
+  {
+    title: 'Mansion in Saratoga Springs, NY',
+    price: '$2,954,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider4,
+    alt: 'House',
+  },
+
+];
+
+export const SliderDataTwo = [
+  {
+    title: 'Mansion in Saratoga Springs, NY',
+    price: '2,954,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider5,
+    alt: 'House',
+  },
+  {
+    title: 'Lake House in Westport, NY',
+    price: '937,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider6,
+    alt: 'House',
+  },
+  {
+    title: 'Estate in Lake George, NY',
+    price: '4,198,100',
+    path: '/homes',
+    label: 'View Home',
+    image: slider7,
+    alt: 'House',
+  },
+  {
+    title: 'Lake House in Saranac Lake, NY',
+    price: '2,750,000',
+    path: '/homes',
+    label: 'View Home',
+    image: slider8,
+    alt: 'House',
+  },
+
+];
