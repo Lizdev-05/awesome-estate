5c8f4c91799b6db890e55941701cfa1f17cfeac4
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-25 18:13:56 +0100

    add award session

---
+++ src/components/Award.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Heading from '../general/Heading';
+import { awards } from '../data/Data';
+import './Award.css';
+
+const Awards = () => (
+  <>
+    <section className="awards padding">
+      <div className="container">
+        <Heading title="Over 1,24,000+ Happy User Bieng With Us Still They Love Our Services" subtitle="Our Awards" />
+
+        <div className="content grid4 mtop">
+          {awards.map((val) => (
+            <div className="box" key={val.id}>
+              <div className="icon">
+                <span>{val.icon}</span>
+              </div>
+              <h1>{val.num}</h1>
+              <p>{val.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default Awards;
