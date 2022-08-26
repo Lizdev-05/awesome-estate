0782b724bc69f72733318a976b16cc7fcb194b56
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-26 15:11:47 +0100

    implemment service component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import img from '../assets/images/services.jpg';
+import Back from '../general/Back';
+import '../featured/Featured.css';
+import FeaturedCard from '../featured/FeaturedCard';
 
 const Services = () => (
-  <div>Services</div>
+  <>
+    <section className="services mb">
+      <Back name="Services" title="Services -All Services" cover={img} />
+      <div className="featured container">
+        <FeaturedCard />
+      </div>
+    </section>
+  </>
 );
 
 export default Services;
